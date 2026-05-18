9807c8de66866284ee069152cc82c2d4547b845b
ursulagarcia-ciber <ursula.garcia@instalacionesgarpi.com>
2026-05-18 09:31:42 +0200

    offline service worker

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE="garpi-v1";
+const ASSETS=["/","/index.html"];
+
+self.addEventListener("install",e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS))
+));
+
+self.addEventListener("fetch",e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const clone=res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,clone));
+    return res;
+  })).catch(()=>caches.match("/index.html"))
+));
