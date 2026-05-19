4c7bc7029dfb2c1cc27611e51fd378643220adc9
ursulagarcia-ciber <ursula.garcia@instalacionesgarpi.com>
2026-05-19 08:29:07 +0200

    fix service worker

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,23 @@
-const CACHE="garpi-v1";
-const ASSETS=["/","/index.html"];
+const CACHE="garpi-v2";
 
-self.addEventListener("install",e=>e.waitUntil(
-  caches.open(CACHE).then(c=>c.addAll(ASSETS))
-));
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(["/","/index.html"]))
+  );
+});
 
-self.addEventListener("fetch",e=>e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    const clone=res.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,clone));
-    return res;
-  })).catch(()=>caches.match("/index.html"))
-));
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("/index.html")))
+  );
+});
